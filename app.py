01b210effb17bc68681bb4f4aa37ab1cd4983b0f
willsf2021 <willsf2021@gmail.com>
2026-02-05 20:33:06 -0300

    feat:'debug=True' scatenato un errore appositamente per utillizzare il debugger nel browser

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Rotte
 @app.route("/")
 def index():
     wow = "bello"
@@ -12,5 +13,15 @@ def info():
     return "<h2>Queste sono un sacoo di informazioni!</h2>"
 
 
+@app.route("/info/<name>") # 127.0.0.1:5000/info/andrea
+def nome_ok(name):
+    return f"<h2>Il mio nome: {name}</h2>"
+
+# Facciamo scattare un errore
+@app.route("/info-errore/<name>")
+def errore(name):
+    return f"<h2>Generiamo un errore: </h2>".format(name[50])
+
+# Avviare l'applicazione
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
