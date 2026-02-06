86981bf37034e0f011b06b7ae903e448af388705
willsf2021 <willsf2021@gmail.com>
2026-02-05 21:19:31 -0300

    feat: altre dei blochi

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -15,7 +15,6 @@ def esempio_jinja():
 def info():
     return "<h2>Queste sono un sacoo di informazioni!</h2>"
 
-
 @app.route("/info/<name>") # 127.0.0.1:5000/info/andrea
 def nome_ok(name):
     return f"<h2>Il mio nome: {name}</h2>"
@@ -24,6 +23,24 @@ def nome_ok(name):
 @app.route("/info-errore/<name>")
 def errore(name):
     return f"<h2>Generiamo un errore: </h2>".format(name[50])
+
+
+
+
+
+@app.route("/course/flask/")
+def course_flask():
+    context = {}
+    context['nome_corso'] = 'Flask'
+    
+    context['corsi'] = [
+       'Introduzione',
+       'Jinja avanzato'
+    ]
+    
+    return render_template("corso-flask.html", context=context)
+
+
 
 # Avviare l'applicazione
 if __name__ == "__main__":
