564a9683c12c77151bd583b696a411aae9021d88
willsf2021 <willsf2021@gmail.com>
2026-02-07 11:51:37 -0300

    feat: creata la struttura basica di un form per creare i corsi e fatto un po' de allenamento di validazione dinamica dei dati

--- app.py
+++ app.py
@@ -1,48 +1,54 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 
-# Rotte
+# Rotta Iniziale
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/esempio/")
-def esempio_jinja():
-    return render_template("flask.html")
-
-
-@app.route("/info/")
-def info():
-    return "<h2>Queste sono un sacoo di informazioni!</h2>"
-
-
-@app.route("/info/<name>")  # 127.0.0.1:5000/info/andrea
-def nome_ok(name):
-    return f"<h2>Il mio nome: {name}</h2>"
-
-
-# Facciamo scattare un errore
-@app.route("/info-errore/<name>")
-def errore(name):
-    return f"<h2>Generiamo un errore: </h2>".format(name[50])
-
-
+# Gestori di Errori
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("404.html"), 404
 
 
-@app.route("/course/flask/")
-def course_flask():
+# Rotte Diversi
+@app.route("/course/new/")
+def new_course():
     context = {}
-    context["nome_corso"] = "Flask"
 
-    context["corsi"] = ["Introduzione", "Jinja avanzato"]
+    args_keys = ["course-name", "course-subject"]
 
-    return render_template("corso-flask.html", context=context)
+    for arg in request.args:
+        if arg in args_keys:
+            key_context = str(arg.replace("-", "_"))
+            context[key_context] = request.args.get(arg)
+
+    # Oppure (Per questo devi conocescere list and dict comprehension):
+    
+    # context.update(
+    #     {
+    #         arg.replace("-", "_"): request.args.get(arg)
+    #         for arg in request.args
+    #         if arg in args_keys
+    #     }
+    # )
+    
+   
+
+    return render_template("course_new.html", context=context)
+
+
+@app.route("/course/created")
+def course_created():
+    context = {}
+    context["course_name"] = "nulla"
+    context["course_subject"] = "nulla"
+
+    return render_template("course_created.html", context=context)
 
 
 # Avviare l'applicazione
